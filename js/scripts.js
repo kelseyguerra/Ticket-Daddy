dad24d8dc7dd585df9b16d89fba50bcc7d28053a
kelsey guerra <kelseylguerra@gmail.com>
2018-04-10 18:25:55 -0700

    add form for name and age

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,16 @@ function Ticket(name, age, movie) {
   this.movie = movie;
 }
 
+
+
+if (inputAge >= 18) {
+  msg = "Sorry, the performance you are trying to attend is 18+"
+} else {
+  msg= "Lucky you! You're 18+!"
+}
+
+
+
 // User Interface Logic
 $(document).ready(function(){
   $("#showtime").submit(function(event) {
@@ -14,8 +24,14 @@ $(document).ready(function(){
     var inputName =
     var inputAge =
     var inputMovie =
+    var returnValue = "go";
+    if (inputAge <= 18) {
+      $("#output").text("Sorry, the performance you are trying to attend is 18+");
+    } else {
+      $("#output").text("Lucky you! You're 18+! Hope you Enjoy the show!");
 
-
+    $('.carousel').carousel(cycle);
+    })
   })
 
   })
