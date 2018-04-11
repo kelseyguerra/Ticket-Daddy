105cf27d19c82b723df7c963df7c4dae4e61db3c
kelsey guerra <kelseylguerra@gmail.com>
2018-04-10 23:16:04 -0700

    change js/still no function

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,37 @@
 // Business Logic
 
-function Ticket(name, age, movie) {
+function Ticket(name, age, concert) {
   this.name = name;
   this.age = age;
-  this.movie = movie;
+  this.concert = concert;
 }
-
+Ticket.prototype.price = function() {
+  return parseInt(this.concert) + 12;
+}
 
 
 // User Interface Logic
-$(document).ready(function(){
-  $("#showtime").submit(function(event) {
+
+$(document).ready(function() {
+  $("#orderForm").submit(function(event) {
     event.preventDefault();
+    var name = $("input#name").val();
+    var age = $("input#age").val();
+    var concert = $("select#concert").val()
+  });
+});
 
-    var inputName = $("#inputName").val();
-    var inputAge = $("#inputAge").val();
-    var inputMovie = parseInt($("input:custom-select[name=concert]:selected").val();
+var newTicket = new Ticket(concert);
+$("#output").text("Total amount: $" + newTicket.price() + ".00");
+});
 
 
-}
-}
+
+
+
+
+
+
 //     var movie = {
 //     showtime: 'inputMovie'
 //     ticketRate: 10,
