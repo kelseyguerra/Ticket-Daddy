775b6061e80c2dcd852c795c1b50b312ec575b7f
kelsey guerra <kelseylguerra@gmail.com>
2018-04-10 21:05:06 -0700

    remove carousel, add js functions still broken

--- js/scripts.js
+++ js/scripts.js
@@ -8,31 +8,24 @@ function Ticket(name, age, movie) {
 
 
 
-if (inputAge >= 18) {
-  msg = "Sorry, the performance you are trying to attend is 18+"
-} else {
-  msg= "Lucky you! You're 18+!"
-}
-
-
-
 // User Interface Logic
 $(document).ready(function(){
   $("#showtime").submit(function(event) {
     event.preventDefault();
 
-    var inputName =
-    var inputAge =
-    var inputMovie =
-    var returnValue = "go";
-    if (inputAge <= 18) {
-      $("#output").text("Sorry, the performance you are trying to attend is 18+");
-    } else {
-      $("#output").text("Lucky you! You're 18+! Hope you Enjoy the show!");
+    var inputName = $("#inputName").val();
+    var inputAge = $("#inputAge").val();
+    var inputMovie = parseInt($("input:custom-select[name=concert]:selected").val();
 
-    $('.carousel').carousel(cycle);
-    })
-  })
 
-  })
-})
+}
+}
+//     var movie = {
+//     showtime: 'inputMovie'
+//     ticketRate: 10,
+//     discount: 5,
+//     offerPrice: function(){
+//       var offerRate = this.movie * ((10 - this.discount) /10)
+//       return offerRate;
+//     }
+//   }:
