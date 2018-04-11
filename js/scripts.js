7fd3362eac460c9c320a5f1953b79c9e968230f7
kelsey guerra <kelseylguerra@gmail.com>
2018-04-11 15:06:05 -0700

    hide output price when under 18

--- js/scripts.js
+++ js/scripts.js
@@ -13,17 +13,17 @@ Ticket.prototype.price = function() {
 // User Interface Logic
 $(document).ready(function() {
   $("#orderForm").submit(function(event) {
-    $('.carousel').carousel()
     event.preventDefault();
     var name = $("input#name").val();
     var age = parseInt($("input#age").val());
     if (age <= 18) {
       alert("Sorry! This show is for age groups 18+!");
+      hide("#output")
     };
     var concert = $('#concert option:selected').val();
     var newTicket = new Ticket(name, age, concert);
 
 
-    $("#output").append("Your Purchase Comes Out To: $" + concert + ".00." + " Please Enjoy Your Show, " + name + );
+    $("#output").append("Your Purchase Comes Out To: $" + concert + ".00." + " Please Enjoy Your Show, " + name);
   })
 });
